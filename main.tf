cc59c710590654566a0b9ecfe7e1d9a954b8b751
nujey <61110132+Yejun4911@users.noreply.github.com>
2025-03-19 13:44:38 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,28 @@ resource "aws_instance" "EC2-TEST" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+# Jenkins-
+# resource "aws_instance" "jenkins_server" {
+#   ami             = var.ami
+#   instance_type   = var.instance_type
+#   # subnet_id       = module.subnet.subnet_id
+#   # security_groups = [module.security_group.sg_id]
+# 
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               sudo yum update -y
+#               sudo yum install -y java-11-openjdk
+#               sudo wget -O /etc/yum.repos.d/jenkins.repo \
+#                 https://pkg.jenkins.io/redhat-stable/jenkins.repo
+#               sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+#               sudo yum install -y jenkins
+#               sudo systemctl enable jenkins
+#               sudo systemctl start jenkins
+#               EOF
+# 
+#   tags = {
+#     Name = "Jenkins-Server"
+#   }
+# }
+}
