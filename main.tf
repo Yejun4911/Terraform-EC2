08dcf301e9e95a1e1f8e503cb5a5bf78abf25c68
nujey <61110132+Yejun4911@users.noreply.github.com>
2025-04-22 15:59:33 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,65 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.29.0"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "EC2-TEST" {
-  ami                   = "ami-0ea766b7a13aecc49"
-  instance_type         = "t4g.micro"
+# # VPC 생성
+# module "vpc" {
+#   source   = "../modules/vpc"
+#   vpc_cidr = "10.0.0.0/16"
+#   name = "dev-dooribun-vpc-apne2"
+# }
+#
+# # 보안 그룹 생성
+module "security_group" {
+  source = "../modules/security-group"
+  # vpc_id = module.vpc.vpc_id
+}
+
+# # 서브넷 생성
+# module "subnet" {
+#   source            = "../modules/subnet"
+#   vpc_id           = module.vpc.vpc_id
+#   subnet_cidr      = "10.0.1.0/24"
+#   availability_zone = "ap-northeast-2a"
+#   public_ip        = true
+#   name = "dev-pub-jenkins-subnet"
+# }
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
+  public_key = file("~/ssh/jenkins-key.pub")
+}
+resource "aws_instance" "jenkins_server" {
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name = aws_key_pair.my_key.key_name
+  # subnet_id       = module.subnet.subnet_id
+  vpc_security_group_ids = [module.security_group.sg_id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              set -e
+
+              # Java 설치
+              sudo dnf install -y java-17-amazon-corretto
+
+              # Jenkins 레포 등록
+              sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              # sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+              sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+              # Jenkins 설치
+              sudo dnf install -y jenkins
+
+              # Jenkins 서비스 실행
+              sudo systemctl enable jenkins
+              sudo systemctl start jenkins
+              EOF
+
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "jenkins-server"
   }
 }
 
-# Jenkins-
-# resource "aws_instance" "jenkins_server" {
-#   ami             = var.ami
-#   instance_type   = var.instance_type
-#   # subnet_id       = module.subnet.subnet_id
-#   # security_groups = [module.security_group.sg_id]
-# 
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               sudo yum update -y
-#               sudo yum install -y java-11-openjdk
-#               sudo wget -O /etc/yum.repos.d/jenkins.repo \
-#                 https://pkg.jenkins.io/redhat-stable/jenkins.repo
-#               sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-#               sudo yum install -y jenkins
-#               sudo systemctl enable jenkins
-#               sudo systemctl start jenkins
-#               EOF
-# 
-#   tags = {
-#     Name = "Jenkins-Server"
-#   }
-# }
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins_server.public_ip
 }
